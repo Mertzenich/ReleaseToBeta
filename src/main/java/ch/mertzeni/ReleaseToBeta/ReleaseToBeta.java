a1e009ffc125d029e1ab53fb4492c08e813f72bf
Adam Mertzenich <adam.mertzenich@gmail.com>
2021-01-05 19:20:26 -0600

    lay groundwork for beta food

--- src/main/java/ch/mertzeni/ReleaseToBeta/ReleaseToBeta.java
+++ src/main/java/ch/mertzeni/ReleaseToBeta/ReleaseToBeta.java
@@ -4,6 +4,7 @@ import ch.mertzeni.ReleaseToBeta.listeners.*;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.command.Command;
 import org.bukkit.plugin.java.annotation.command.Commands;
+import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
 import org.bukkit.plugin.java.annotation.plugin.Description;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
 import org.bukkit.plugin.java.annotation.plugin.author.Author;
@@ -12,6 +13,7 @@ import org.bukkit.plugin.java.annotation.plugin.author.Author;
 @Description(value = "Taking modern minecraft back to simpler times.")
 @Author(value = "Adam Mertzenich")
 @Commands(@Command(name = "r2b", desc = "ReleaseToBeta Command"))
+@ApiVersion(ApiVersion.Target.v1_15)
 
 public class ReleaseToBeta extends JavaPlugin {
     @Override
--- src/main/java/ch/mertzeni/ReleaseToBeta/listeners/FoodHandler.java
+++ src/main/java/ch/mertzeni/ReleaseToBeta/listeners/FoodHandler.java
@@ -1,10 +1,13 @@
 package ch.mertzeni.ReleaseToBeta.listeners;
 
 import ch.mertzeni.ReleaseToBeta.ReleaseToBeta;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class FoodHandler implements Listener {
 
@@ -13,8 +16,31 @@ public class FoodHandler implements Listener {
         this.plugin = plugin;
     }
 
+    public static double healAmount(double playerHealth, double healAmount) {
+        if (playerHealth + healAmount < 20) {
+            return playerHealth + healAmount;
+        }else {
+            return 20;
+        }
+    }
 
-    // Disallow placing items in off-hand slot
+
+
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e) {
+        e.getPlayer().sendMessage(e.getItem().toString() + " " + e.getAction().toString() + " " + e.getItem().getType().toString());
+        Player player = e.getPlayer();
+        double playerHealth = player.getHealth();
+
+        if (e.getItem().getType() == Material.COOKED_PORKCHOP) {
+            player.setHealth(healAmount(playerHealth, 8));
+            player.sendMessage(String.valueOf(healAmount(playerHealth, 8)));
+        }
+
+    }
+
+    // Disallow consuming food (probably won't ever fire when everything else is done)
     @EventHandler
     public void onConsume(PlayerItemConsumeEvent e) {
         e.setCancelled(true);
